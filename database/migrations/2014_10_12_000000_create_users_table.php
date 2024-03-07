585d9e7ab8d9fe760b290a741b31d70fcc2e24a5
wildanibs25 <sin990325@gmail.com>
2024-03-07 17:10:58 +0700

    users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
-            $table->string('fullname');
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('is_admin')->default(0);
